965c5d8e30f83dbe1643c430b5de6b07cdfe8228
Rian Duarte Matos de Oliveira <123524730+riandte@users.noreply.github.com>
2025-07-13 18:13:36 -0300

    TESTE

--- src/config/db.js
+++ src/config/db.js
@@ -10,7 +10,8 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  port:     Number(process.env.DB_PORT),
+  ssl:      { rejectUnauthorized: false },
 });
 
 pool.connect()
